568862951585e6f72163eff6a2f9fe3d5ad3efc3
Adam Carlile <adam@benchmedia.co.uk>
2014-06-23 18:07:04 +0100

    BUILD ME TO THE MOON, LET ME PLAY AMONGST THE STARS

--- lib/docket/builder.rb
+++ lib/docket/builder.rb
@@ -2,10 +2,10 @@ module Docket
   class Builder
 
     def initialize(file_path, config={})
-      @sns_connection = Aws.sns(connection_options.merge(config[:sns]))
-      @sqs_connection = Aws.sqs(connection_options.merge(config[:sqs]))
+      @sns_connection = Aws.sns(connection_options.merge((config[:sns] || {})))
+      @sqs_connection = Aws.sqs(connection_options.merge((config[:sqs] || {})))
 
-      @manifest = YAML.load(file_path)[Docket.env].with_indifferent_access
+      @manifest = YAML.load(File.read(file_path))[Docket.env].with_indifferent_access
     end
 
     def build!
@@ -15,8 +15,8 @@ module Docket
 
     def build_sqs
       @manifest[:sqs].each do |queue|
-        puts "Creating queue #{queue.to_s.dasherize}-#{Docket.env.downcase}"
-        @sqs_connection.create_queue([queue[:name].to_s.dasherize, Docket.env.downcase].join('-'))
+        puts "Creating queue #{queue[:name].to_s.dasherize}-#{Docket.env.downcase}"
+        @sqs_connection.create_queue(queue_name: [queue[:name].to_s.dasherize, Docket.env.downcase].join('-'))
         #
         # I think there will need to be a loop here
         #
@@ -25,19 +25,21 @@ module Docket
 
     def build_sns
       @manifest[:sns].each do |topic|
-        arn = @sns_connection.create_topic [topic[:topic].to_s.dasherize, Docket.env.downcase].join('-'))
+        arn = @sns_connection.create_topic(name: [topic[:topic].to_s.dasherize, Docket.env.downcase].join('-')).topic_arn
         puts "Create #{arn}"
         topic[:subscriptions].each do |sub|
-          if sub[:protocol] == 'sqs' || sub[:protocol] == 'cqs'
-            endpoint = @sqs_connection.list_queues.queue_urls.detect {|x| x.match /#{sub[:endpoint].to_s.dasherize}-#{Docket.env.downcase}/ }
+          if sub[:protocol] == :sqs || sub[:protocol] == :cqs
+            url = @sqs_connection.list_queues.queue_urls.detect {|x| x.match /#{sub[:endpoint].to_s.dasherize}-#{Docket.env.downcase}/ }
+            endpoint = @sqs_connection.get_queue_attributes(queue_url: url, attribute_names: ['QueueArn']).attributes['QueueArn']
           else
             endpoint = sub[:endpoint]
           end
           puts "Create #{endpoint}"
-          sub_arn = @sns_connection.subscribe(topic_arn: arn, protocol: sub[:protocol], endpoint: endpoint)
+          sub_arn = @sns_connection.subscribe(topic_arn: arn, protocol: sub[:protocol], endpoint: endpoint).subscription_arn
           sub[:attributes].each do |key, value|
+            next if key.blank? || value.blank?
             puts "Setting #{key} on #{sub_arn}"
-            @sns_connection.set_subscription_attributes(subscription_arn: sub_arn, attribute_name: key, attribute_value: value)
+            @sns_connection.set_subscription_attributes(subscription_arn: sub_arn, attribute_name: key.to_s, attribute_value: value.to_s)
           end
         end
       end
