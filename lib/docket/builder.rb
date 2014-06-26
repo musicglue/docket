67bfec659f5cb0ac764ff1aba787d3879dd6de65
Lee Henson <lee.m.henson@gmail.com>
2014-06-26 17:33:43 +0100

    don't explode when subscriptions are not defined for a topic

--- lib/docket/builder.rb
+++ lib/docket/builder.rb
@@ -59,7 +59,7 @@ module Docket
 
         say_status :success, "Topic #{arn} created successfully", :green
 
-        topic[:subscriptions].each do |sub|
+        (topic[:subscriptions] || []).each do |sub|
           if sub[:protocol] == :sqs || sub[:protocol] == :cqs
             url = @sqs_connection.list_queues.queue_urls.detect {|x| x.match /#{sub[:endpoint].to_s.dasherize}-#{Docket.env.downcase}/ }
             endpoint = @sqs_connection.get_queue_attributes(queue_url: url, attribute_names: ['QueueArn']).attributes['QueueArn']
